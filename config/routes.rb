5b4ca205987cb930740064fc5426184d99811539
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-31 11:05:00 +0000

    ログイン、新規登録の仮実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   root to: 'home#top'
-  get 'home/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about' => 'home#about'
+  
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions      => 'users/sessions'
+  }
   
 end
