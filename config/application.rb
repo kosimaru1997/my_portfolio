db8a7d948719587a03fb0c3b93aa918e50c2270a
kosimaru1997 <kosimaru1997@gmail.com>
2021-06-10 15:18:44 +0000

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Portfolio
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
