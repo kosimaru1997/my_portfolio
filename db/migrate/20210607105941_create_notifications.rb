5393642a0d992841619cd665a2ff8c51e3eae448
kosimaru1997 <kosimaru1997@gmail.com>
2021-06-20 14:27:43 +0000

    fix migration important_change

--- db/migrate/20210607105941_create_notifications.rb
+++ db/migrate/20210607105941_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
       t.references :visitor, foreign_key: { to_table: :users }, null: false
-      t.integer :visited_id, foreign_key: { to_table: :users }, null: false
+      t.references :visited_id, foreign_key: { to_table: :users }, null: false
       t.integer :post_id, foreign_key: true
       t.integer :post_comment_id, foreign_key: true
       t.string :action, default: '', null: false
