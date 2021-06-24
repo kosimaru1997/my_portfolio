104ae8ab4f3f4c664bae6d3044b805e8bf43821d
kosimaru1997 <kosimaru1997@gmail.com>
2021-06-24 16:45:44 +0000

    finish before_login test

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
-describe "Users before_login test" do
+describe "Users sign_up log_in log_out test" do
 
   describe "Users sign up", type: :system do
     before do
       visit root_path
     end
 
-    it "user successfully sign up" do
+    it "correct link before sign_up" do
+      expect(page).to have_link "", href: "/"
+      expect(page).to have_link "", href: "/unlogin/posts"
+      expect(page).to have_link "", href: "/unlogin/users"
+      expect(page).to have_link "お問い合わせ", href: "/inquiry"
+    end
+
+    it "user successfully sign_up" do
       within("#sign-up") do
         expect {
           fill_in "user[name]", with: "テスト"
@@ -22,7 +29,7 @@ describe "Users before_login test" do
       expect(page).to have_content "テスト"
     end
 
-    it "user failure sign up", js: true do
+    it "user failure sign_up", js: true do
         within("#sign-up") do
         expect {
           fill_in "user[name]", with: "テスト"
@@ -38,21 +45,30 @@ describe "Users before_login test" do
     end
   end
 
-  describe "Users log in" do
+  describe "Users log_in" do
     let(:user) { create(:user) }
 
     before do
       visit root_path
     end
 
-    it "user successfully log in" do
+    it "user successfully log_in and log_out" do
       within("#login") do
         fill_in "user[email]", with: user.email
         fill_in "user[password]", with: user.password
         click_button "Log in"
       end
       expect(current_path).to eq "/"
-      expect(page).to have_content user.name
+      expect(page).to have_link "", href: "/"
+      expect(page).to have_link "", href: "/posts"
+      expect(page).to have_link "", href: "/notifications"
+      expect(page).to have_link "", href: "/rooms"
+      expect(page).to have_link "マイページ", href: "/users/#{user.id}"
+      expect(page).to have_link "ユーザー一覧", href: "/users"
+      expect(page).to have_link "ログアウト", href: "/users/sign_out"
+      expect(page).to have_link "お問い合わせ", href: "/inquiry"
+      click_link "ログアウト"
+      expect(page).to have_link "", href: "/unlogin/posts"
     end
 
     it "user failure log in" do
@@ -62,7 +78,7 @@ describe "Users before_login test" do
         click_button "Log in"
       end
         expect(current_path).to eq "/"
-        expect(page).to have_no_content user.name
+        expect(page).to have_no_link "マイページ", href: "/users/#{user.id}"
     end
   end
 
