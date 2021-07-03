d5db12b628bc1783b4490370d8d6c366657db81e
kosimaru1997 <kosimaru1997@gmail.com>
2021-07-03 14:51:26 +0000

    Refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
 require 'uri'
 
+def show_contents_with_uri(contents)
+  text = safe_join(contents.split("\n"),tag(:br))
+  text.gsub(URI.regexp(['http', 'https'])) do |text|
+    "<object><a href='#{text}'>#{text}</a></object>"
+  end
+end
+
 def convert_url_into_a_tag(text)
   text.gsub(URI.regexp(['http', 'https'])) do |text|
     "<object><a href='#{text}'>#{text}</a></object>"
